2053d71ee4fd1e85a2d94202a62b15daf992aa07
Gabriel Ibarra <23041028@itdurango.edu.mx>
2026-09-13 01:03:41 -0600

    Corrige STATIC_ROOT para Render

--- practica05_DyangoMVT/settings.py
+++ practica05_DyangoMVT/settings.py
@@ -122,3 +122,5 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+# Configuración de archivos estáticos para producción
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
